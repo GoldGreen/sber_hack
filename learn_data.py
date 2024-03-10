ea62610af3d9039fb641d439a45266693b45d085
GoldGreen <48961363+GoldGreen@users.noreply.github.com>
2024-03-10 13:37:56 +0300

    add model choosing

--- learn_data.py
+++ learn_data.py
@@ -1,30 +1,47 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import f1_score, precision_score, recall_score, roc_auc_score
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import f1_score, roc_auc_score,accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from tqdm.auto import tqdm
 
 df = pd.read_parquet('data/processed_data.parquet', engine='fastparquet')
 
 target_col = 'target'
 feature_cols = [column for column in df.columns if column not in [target_col] ]
 
-
 y = df[target_col] 
 x = df[feature_cols]
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-model =  RandomForestClassifier(n_estimators=100, class_weight='balanced')
-model.fit(x_train, y_train)
+models = [
+    GradientBoostingClassifier(),
+    RandomForestClassifier()
+]
 
-y_pred = model.predict(x_test)
+max_features = ['sqrt', 51]
 
-precision = precision_score(y_test, y_pred)
-recall = recall_score(y_test, y_pred)
-f1 = f1_score(y_test, y_pred)
-roc_auc = roc_auc_score(y_test, y_pred)
+gb_param_grid = { 'n_estimators': [50, 100, 200], 'learning_rate':[0.01], 'max_features':max_features}
+rf_param_grid = { 'n_estimators': [100, 200, 300], 'max_features':max_features}
 
-print(f"Precision: {precision}")
-print(f"Recall: {recall}")
-print(f"F1 Score: {f1}")
-print(f"ROC AUC Score: {roc_auc}")
+param_grids = [gb_param_grid,rf_param_grid]
+
+for model, param_grid in zip(models,param_grids):
+    grid_search = GridSearchCV(model, param_grid, scoring='roc_auc', cv=3, verbose=10)
+    grid_search.fit(x_train, y_train)
+    
+    best_model = grid_search.best_estimator_
+    best_params = grid_search.best_params_
+    
+    y_pred = best_model.predict(x_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    roc_auc = roc_auc_score(y_test, y_pred)
+
+    print(f'Model: {type(model).__name__}, best params: {best_params}')
+    print(f"Accuracy Score: {accuracy:.2f}")
+    print(f"F1 Score: {f1:.2f}")
+    print(f"Roc_auc Score: {roc_auc:.2f}\n")
